02a5cae5d6abd5a15be313c6a03a2029a5357ca8
Mariah <alinemariah@hotmail.com>
2025-06-10 17:53:59 +0200

    feat: install @heroicons/react to have the moon Icon and add it to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
+import { MoonIcon } from "@heroicons/react/24/solid"
 
 const Header = () => {
     return ( 
         <div className="flex h-20 bg-gray-400 justify-between items-center px-5 sm:rounded-xl sm:m-5">
             <span className="text-gray-100"> Hello, User!</span>
-            <h1 className="text-gray-100 "> Mariah Newsletter</h1>
-            <input className="hidden sm:block" type="checkbox" />
+            <h1 className="text-gray-100 text-xl "> Mariah Newsletter</h1>
+            <MoonIcon className="h-8 text-gray-100 hidden sm:block cursor-pointer"/>
+           
+          {/*   <input className="hidden sm:block" type="checkbox" /> */}
 
-            <div class="p-8">
-                
-  <div class="hidden sm:block bg-green-300 p-4 rounded">
-    ✅ Só aparece em telas maiores que 640px (tablet ou desktop)
-  </div>
+   {/*          <div className="p-8">
+                  <div className="hidden sm:block bg-green-300 p-4 rounded">
+                    ✅ Só aparece em telas maiores que 640px (tablet ou desktop)
+                  </div>
 
-  <div class="block sm:hidden bg-red-300 p-4 rounded mt-4">
-    📱 Só aparece em telas menores que 640px (mobile)
-  </div>
-</div>
+                  <div className="block sm:hidden bg-red-300 p-4 rounded mt-4">
+                    📱 Só aparece em telas menores que 640px (mobile)
+                  </div>
+                </div> */}
 
           
         </div>
 
-     );
+     
+);
 }
  
 export default Header;
